0c9041cb0e4f5a17bd505fddadae6ae4deccaec8
czz <18225372273@163.com>
2021-07-02 22:28:21 +0800

    项目基础环境

---
+++ src/main/java/com/baizhi/Git2010Application.java
@@ -0,0 +1,13 @@
+package com.baizhi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Git2010Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Git2010Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/baizhi/Git2010ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baizhi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Git2010ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
